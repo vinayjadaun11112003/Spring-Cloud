af49ce38c24d29efc3bfdc8803ed6b6c36de6862
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-03-23 21:52:50 +0530

    [Vinay Jadaun] - Add. Config Server Integrated in the application

---
+++ Spring-Cloud-main/SpringCloud_Hotel_application/ConfigServer/src/test/java/com/Server/ConfigServier/ConfigServierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Server.ConfigServier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
